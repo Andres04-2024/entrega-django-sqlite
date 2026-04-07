69c3a276502698ff89ecd19ce4d8fef2e83d3db5
Andres04-2024 <andresraffaeli33@gmail.com>
2026-04-07 07:18:45 -0300

    Actividad 1: API de Roles finalizada

--- saludo/urls.py
+++ saludo/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
-from .views import hola_mundo
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import hola_mundo, RolViewSet
+
+router = DefaultRouter()
+router.register(r'roles', RolViewSet)
 
 urlpatterns = [
     path('', hola_mundo),
+    path('api/', include(router.urls)),
 ]
--- saludo/views.py
+++ saludo/views.py
@@ -2,3 +2,12 @@ from django.http import HttpResponse
 
 def hola_mundo(request):
     return HttpResponse("Hola Mundo!!")
+
+
+from rest_framework import viewsets
+from .models import Rol
+from .serializers import RolSerializer
+
+class RolViewSet(viewsets.ModelViewSet):
+    queryset = Rol.objects.all()
+    serializer_class = RolSerializer
